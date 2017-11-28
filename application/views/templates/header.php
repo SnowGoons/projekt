443da308ec493bb54ce2462ed1e6ffc49cc04c86
SnowGoons <dominik51423@wp.pl>
2017-11-28 20:34:25 +0100

    *

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,8 @@
 <html>
 <head>
         <title>Aukcjoner</title>
-    <link rel="stylesheet" href="<? base_url();?>/css/bootstrap.css">
+    <link rel="stylesheet" href="<? = base_url();?>/css/bootstrap.css">
 </head>
-<body style="background-color: linear-gradient(darkred 10%, greey 90%)">
+<body style="background-color: aqua">
     <h1>Wkrótce - navbar</h1>
 </div>
