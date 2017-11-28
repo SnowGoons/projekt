125411260040f88ae40d1a6c6ba6c799df8cd6b6
Archim26 <lwiniarczyk26@gmail.com>
2017-11-28 20:42:34 +0100

    zmiany nadchodzo

--- application/controllers/rejestracja.php
+++ application/controllers/rejestracja.php
@@ -3,19 +3,19 @@ class Rejestracja extends CI_Controller {
 
   public function index()
   {
-    $this->load->helper(array('rejestracja', 'url'));
+    $this->load->helper(array('form', 'url'));
 
     $this->load->library('form_validation');
 
-    $this->form_validation->set_rules('nick','Nick','wymagany');
-    $this->form_validation->set_rules('haslo','Haslo','wymagane');
-    $this->form_validation->set_rules('potwhaslo','Potwierdzenie Hasla','wymagane');
-    $this->form_validation->set_rules('email','Email','wymagany');
-    $this->form_validation->set_rules('imie','Imie','wymagane');
-    $this->form_validation->set_rules('nazwisko','Nazwisko','wymagane');
-    $this->form_validation->set_rules('kodpocz','Kod Pocztowy','wymagany');
-    $this->form_validation->set_rules('miasto','Miejscowosc','wymagana');
-    $this->form_validation->set_rules('ulica','Ulica','wymagana');
+    $this->form_validation->set_rules('nick','Nick','required');
+    $this->form_validation->set_rules('haslo','Haslo','required');
+    $this->form_validation->set_rules('potwhaslo','Potwierdzenie Hasla','required');
+    $this->form_validation->set_rules('email','Email','required');
+    $this->form_validation->set_rules('imie','Imie','required');
+    $this->form_validation->set_rules('nazwisko','Nazwisko','required');
+    $this->form_validation->set_rules('kodpocz','Kod Pocztowy','required');
+    $this->form_validation->set_rules('miasto','Miejscowosc','required');
+    $this->form_validation->set_rules('ulica','Ulica','required');
 
     if ($this->form_validation->run()==FALSE)
     {
