bd36080e192d93d522a4a2de5042b0cd9a1295f1
Archim26 <lwiniarczyk26@gmail.com>
2017-11-28 21:41:53 +0100

    emilek mial racje

--- application/controllers/rejestracja.php
+++ application/controllers/rejestracja.php
@@ -7,13 +7,13 @@ class Rejestracja extends CI_Controller {
 
     $this->load->library('form_validation');
 
-    $this->form_validation->set_rules('nick','Nick','required|min_length[5]|max_length[16]|is_unique[projekt.nick]',
+    $this->form_validation->set_rules('nick','Nick','required|min_length[5]|max_length[16]|is_unique[uzytkownicy.nick]',
     array('required'=>'Nie wprowadziles/as nick`u.','is_unique'=>'Ten nick juz istnieje.'));
     $this->form_validation->set_rules('haslo','Haslo','required|min_length[4]|max_length[12]',
   array('required'=>'Nie wprowadziles/as hasla.'));
     $this->form_validation->set_rules('potwhaslo','Potwierdzenie Hasla','required|matches[haslo]',
   array('required'=>'Musisz potwierdzic haslo.','matches'=>'Hasla sie nie zgadzaja') );
-    $this->form_validation->set_rules('email','Email','required|valid_email|is_unique[projekt.email]',
+    $this->form_validation->set_rules('email','Email','required|valid_email|is_unique[uzytkownicy.email]',
   array('required'=>'Nie wprowadziles/as adresu email.','valid_email'=>'Adres email musi byc wazny.','is_unique'=>'Adres email juz istnieje.'));
     $this->form_validation->set_rules('imie','Imie','required',
   array('required'=>'Nie wprowadziles/as imienia.'));
