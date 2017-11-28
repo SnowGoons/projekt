e62aff32fa75e40f2785104d02a6aab7598e0933
SnowGoons <dominik51423@wp.pl>
2017-11-28 18:39:48 +0100

    18:39

---
+++ application/views/templates/footer.php
@@ -0,0 +1,6 @@
+
+<div id="cat" class="col-sm-12">
+    <h1 id="footer">Projekt tworzą/-li Dominik Mucha & Łukasz Winiarczyk</h1>
+</div>
+    </boody>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+        <title>Aukcjoner</title>
+    <link rel="stylesheet" href="<?= base_url();?>/css/bootstrap.css">
+</head>
+<body style="background: linear-gradient(darkred 10%, greey 90%)">
+<div id="cat" class="col-md-12">
+    <h1>Wkrótce - navbar</h1>
+</div>
