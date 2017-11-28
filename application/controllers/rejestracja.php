1ac963475a6166f6cff53e77fbbbaa3ad9a30f1c
Archim26 <lwiniarczyk26@gmail.com>
2017-11-28 20:12:20 +0100

    zasaady
    ewa

--- application/controllers/rejestracja.php
+++ application/controllers/rejestracja.php
@@ -7,6 +7,16 @@ class Rejestracja extends CI_Controller {
 
     $this->load->library('form_validation');
 
+    $this->form_validation->set_rules('nick','Nick','wymagany');
+    $this->form_validation->set_rules('haslo','Haslo','wymagane');
+    $this->form_validation->set_rules('potwhaslo','Potwierdzenie Hasla','wymagane');
+    $this->form_validation->set_rules('email','Email','wymagany');
+    $this->form_validation->set_rules('imie','Imie','wymagane');
+    $this->form_validation->set_rules('nazwisko','Nazwisko','wymagane');
+    $this->form_validation->set_rules('kodpocz','Kod Pocztowy','wymagany');
+    $this->form_validation->set_rules('miasto','Miejscowosc','wymagana');
+    $this->form_validation->set_rules('ulica','Ulica','wymagana');
+
     if ($this->form_validation->run()==FALSE)
     {
       $this->load->view('myform');
