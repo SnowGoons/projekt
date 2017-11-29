410b455a6197fb72d6274b0aea00b866eeb9609a
Archim26 <lwiniarczyk26@gmail.com>
2017-11-29 18:00:25 +0100

    repopulacja... taka sytuacja

--- application/views/rejestracja.php
+++ application/views/rejestracja.php
@@ -8,31 +8,31 @@
   <?php echo form_open('rejestracja'); ?>
 
   <h5>Nick</h5>
-  <input type="text" name="nick" value="" size="16" />
+  <input type="text" name="nick" value="<?php set_value('nick'); ?>" size="16" />
 
   <h5>Haslo</h5>
-  <input type="text" name="haslo" value="" size="12" />
+  <input type="text" name="haslo" value="<?php set_value('haslo'); ?>" size="12" />
 
   <h5>Potwierdz Haslo</h5>
-  <input type="text" name="potwhaslo" value="" size="12" />
+  <input type="text" name="potwhaslo" value="<?php set_value('potwhaslo'); ?>" size="12" />
 
   <h5>Adres Email</h5>
-  <input type="text" name="email" value="" size="50" />
+  <input type="text" name="email" value="<?php set_value('email'); ?>" size="50" />
 
   <h5>Imie</h5>
-  <input type="text" name="imie" value="" size="16" />
+  <input type="text" name="imie" value="<?php set_value('imie'); ?>" size="16" />
 
   <h5>Nazwisko</h5>
-  <input type="text" name="nazwisko" value="" size="50" />
+  <input type="text" name="nazwisko" value="<?php set_value('nazwisko'); ?>" size="50" />
 
   <h5>Kod Pocztowy</h5>
-  <input type="text" name="kodpocz" value="" size="6" />
+  <input type="text" name="kodpocz" value="<?php set_value('kodpocz'); ?>" size="6" />
 
   <h5>Miejscowosc</h5>
-  <input type="text" name="miasto" value="" size="50" />
+  <input type="text" name="miasto" value="<?php set_value('miasto'); ?>" size="50" />
 
   <h5>Ulica</h5>
-  <input type="text" name="ulica" value="" size="32" />
+  <input type="text" name="ulica" value="<?php set_value('ulica'); ?>" size="32" />
 
   <div><input type="submit" name="Submit" /></div>
 
