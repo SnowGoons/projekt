ef07efd938fe60ab6d6ba5bd82f7db1d117405c1
Archim26 <lwiniarczyk26@gmail.com>
2017-12-01 21:39:51 +0100

    lekkie poprawki

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -10,7 +10,7 @@
         <label for="haslo">Haslo</label>
         <input type="password" name="haslo">
         <span class="text-danger"><?= form_error('password');?></span><br />
-        <input class="btn btn-primary" type="submit" value="Login">
+        <input class="btn btn-primary" type="submit" value="Login" style="margin: 5 95 0;">
         <?= '<label class="text-danger">'.$this->session->flashdata("error").'</label>'; ?>
 
     </form>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,6 @@
 
 <div id="cat" class="col-sm-12">
-    <h1 style="clear: both; position: fixed; bottom: 0; font-size: 10; color: rgba (0,0,0,0.6)" id="footer">Projekt tworzą/-li Dominik Mucha & Łukasz Winiarczyk</h1>
+    <h1 style="clear: both; position: fixed; bottom: 0; font-size: 12; color: rgba (0,0,0,0.6); border-bottom: 1px solid #D0D0D0;" id="footer">Projekt tworzą/-li Dominik Mucha & Łukasz Winiarczyk</h1>
 </div>
     </boody>
 </html>
