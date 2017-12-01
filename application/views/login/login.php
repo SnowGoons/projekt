4f5628d3acc0bed6569485c4bb46289c93eddc26
SnowGoons <dominik51423@wp.pl>
2017-12-01 20:27:42 +0100

    *

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,18 +1,19 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <div id="cat" class="col-md-4 col-md-offset-4">
-
+<div class="row">
     <?php echo form_open('Login'); ?>
 
         <label for="nick">Login</label>
         <input type="text" name="nick">
-        <span class="text-danger"><?= form_error('username');?></span>
+        <span class="text-danger"><?= form_error('username');?></span><br />
         <label for="haslo">Haslo</label>
         <input type="password" name="haslo">
-        <span class="text-danger"><?= form_error('password');?></span>
+        <span class="text-danger"><?= form_error('password');?></span><br />
         <input class="btn btn-primary" type="submit" value="Login">
         <?= '<label class="text-danger">'.$this->session->flashdata("error").'</label>'; ?>
 
     </form>
 
+</div>
 </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,13 +4,15 @@
     <link rel="stylesheet" href="./css/bootstrap.css">
 </head>
 <body style="background-color: #CC9933">
+  <div class="container" style="
+    margin-top: 50px; ">
   <div class="container">
 <div class="navbar-header">
     <nav class="navbar navbar-inverse navbar-fixed-top"
   style=" background: linear-gradient(darkred 20%, black 80%);">
 
-      <a href="" class="navbar-brand"><spam class="glyphicon glyphicon-user">Zaloguj</a>
-    <a href="" class="navbar-brand"><spam class="glyphicon glyphicon-user">Rejestracja</a>
+      <a href="login" class="navbar-brand"><spam class="glyphicon glyphicon-user">Zaloguj</a>
+    <a href="rejestracja" class="navbar-brand"><spam class="glyphicon glyphicon-user">Rejestracja</a>
   </nav>
 
 </div>
